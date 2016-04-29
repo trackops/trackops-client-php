93bd93c1f395369caa7b973a8358e9c27bba7408
Ian Ricketson <iricketson@gmail.com>
2016-04-29 17:19:59 -0600

    Add test stubs

---
+++ tests/GuzzleRequestTest.php
@@ -0,0 +1,9 @@
+<?php
+namespace Trackops\Test;
+
+use Trackops\Api\GuzzleRequest;
+
+class GuzzleRequestTest extends \PHPUnit_Framework_TestCase
+{
+    // test stubs
+}
---
+++ tests/GuzzleResponseTest.php
@@ -0,0 +1,9 @@
+<?php
+namespace Trackops\Test;
+
+use Trackops\Api\GuzzleResponse;
+
+class GuzzleResponseTest extends \PHPUnit_Framework_TestCase
+{
+    // test stubs
+}
